2aa33c493490cdf0408a1fc006a19a7126aedb43
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-05-03 11:32:08 +0100

    Add: Timetable draft to endpoints

--- src/routes/timetable.ts
+++ src/routes/timetable.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { createTimetable, getTimetableByHash } from "../controllers/timetable";
+import {
+  createTimetable,
+  draftTimetable,
+  getTimetableByHash,
+} from "../controllers/timetable";
 
 const router = Router();
 
 router.post("/", createTimetable);
-// router.post("/draft", dratTimetable);
+router.post("/draft", draftTimetable);
 router.get("/:hash", getTimetableByHash);
 
 export default router;

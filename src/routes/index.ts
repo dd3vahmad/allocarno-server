37854d68fbe5483c38e432fa559be9a629731969
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-05-03 09:27:19 +0100

    Feat: Expose halls, student-group and lecturers route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
-// import userRoutes from "./user";
-// import messagesRoutes from "./message";
+import hallsRoutes from "./hall";
+import lecturersRoutes from "./lecturer";
 import timetablesRoutes from "./timetable";
 import authRoutes from "./auth";
 import { Router } from "express";
 
 const protectedRoutes = Router();
 
-// protectedRoutes.use("/users", userRoutes);
-// protectedRoutes.use("/messages", messagesRoutes);
+protectedRoutes.use("/halls", hallsRoutes);
+protectedRoutes.use("/lecturers", lecturersRoutes);
 protectedRoutes.use("/timetables", timetablesRoutes);
 
 export { authRoutes, protectedRoutes };

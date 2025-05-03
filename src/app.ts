984dcaef469d0aa73506a6a0529de7f1f09262f1
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-05-03 02:32:19 +0100

    Add: Views file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import scheduleRoutes from "./routes/timetable";
 import connectDB from "./config/db";
 
 dotenv.config();
@@ -11,7 +10,28 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/schedule", scheduleRoutes);
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/v1/", authenticate, protectedRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = 500;
+  const message = err.message || "Internal Server Error";
+
+  // I don't know what void means but I hate unused code...
+  void next;
+  void req;
+
+  return res.status(statusCode).json({ message, failed: true }) as any;
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.render("i", {
+    title: `Hi ${req.hostname}! Welcome to Allocarno Backend.`,
+  });
+});
 
 connectDB().then(() => {
   app.listen(PORT, () => {

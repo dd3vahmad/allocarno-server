70b9c9d1dc050eeaa128c49472647c5c8f749ef9
Akanbi AbdulAzeez <109679217+Harziz712@users.noreply.github.com>
2025-05-30 07:26:48 -0400

    Update student.ts route to access controller

--- src/routes/student.ts
+++ src/routes/student.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 
-router.push("/");
-router.get("/");
-router.get("/:id");
+import {
+  addStudent,
+  getStudents
+} from "../controllers/student";
+
+const router = Router();
+
+router.get("/", getStudents);
+router.post("/", addStudent);
 
 export default router;
